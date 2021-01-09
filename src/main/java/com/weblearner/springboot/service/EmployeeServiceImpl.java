6cefe1619f8e7b64454d92908f1e52d18fdf99f8
Raghu Seetharaman <ramyaraghu2510@gmail.com>
2021-01-08 21:35:54 -0500

    First commit

--- src/main/java/com/weblearner/springboot/service/EmployeeServiceImpl.java
+++ src/main/java/com/weblearner/springboot/service/EmployeeServiceImpl.java
@@ -3,9 +3,9 @@ package com.weblearner.springboot.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
-
 import org.springframework.stereotype.Component;
-
+import com.weblearner.springboot.aop.CalculatePerformance;
+import com.weblearner.springboot.aop.MethodLogger;
 import com.weblearner.springboot.model.Employee;
 
 @Component
@@ -14,38 +14,52 @@ public class EmployeeServiceImpl implements EmployeeService {
 	private static List<Employee> employeeLst = new ArrayList<>();
 
 	@Override
+	@CalculatePerformance
 	public Employee getEmployee(String empId) {
 		Stream<Employee> empStream = employeeLst.stream().filter(emp -> {
 			return emp.getEmpId().equalsIgnoreCase(empId);
 		});
+		sleepForSeconds(5000L);
 		return empStream.findAny().get();
 	}
 
 	@Override
+	@MethodLogger
 	public String addEmployee(Employee e) {
 		employeeLst.add(e);
+		sleepForSeconds(3000L);
 		return "Success";
 	}
 
 	@Override
+	@MethodLogger
 	public String updateEmployeeDept(String empId, String deptName) {
-
 		employeeLst.stream().forEach(emp -> {
 			if (emp.getEmpId().equalsIgnoreCase(empId)) {
 				emp.setDeptName(deptName);
 			}
 		});
+		sleepForSeconds(2000L);
 
 		return "SUCCESS";
 	}
 
 	@Override
+	@MethodLogger
 	public String deleteEmployee(String empId) {
-		System.out.println("Employee Id -->"+ empId);
+		System.out.println("Employee Id -->" + empId);
 		if (employeeLst.removeIf(emp -> emp.getEmpId().equalsIgnoreCase(empId)))
 			return "SUCCESS";
 		else
 			return "FAIURE";
+	}
+
+	public void sleepForSeconds(Long period) {
+		try {
+			Thread.sleep(period);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	static {
